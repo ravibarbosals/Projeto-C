ba4ccd7b52d36928c35b4b9685af7c7a4b111702
Pedro <pedrocarvalhog10@gmail.com>
2025-05-19 21:54:02 +0000

    alteração do keyboard.h

--- Projeto-C-main/include/keyboard.h
+++ Projeto-C-main/include/keyboard.h
@@ -1,13 +1,3 @@
-/**
- * keyboard.h
- * Created on Aug 23rd, 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
- * 
- * This header provides basic keyboard input functionality
- * for Windows using <conio.h>
- */
-
 #ifndef __KEYBOARD_H__
 #define __KEYBOARD_H__
 
@@ -15,26 +5,9 @@
 extern "C" {
 #endif
 
-/**
- * Initializes keyboard input system.
- */
 void keyboardInit();
-
-/**
- * Restores keyboard input system.
- */
 void keyboardDestroy();
-
-/**
- * Checks if a key has been pressed.
- * @return non-zero if a key is available to read, 0 otherwise.
- */
 int keyhit();
-
-/**
- * Reads a character from keyboard input.
- * @return the character read from input.
- */
 int readch();
 
 #ifdef __cplusplus
